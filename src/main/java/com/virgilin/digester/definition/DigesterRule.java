2337e5f2c42a9d424801664c1fa03e71b219f8b7
Virgiljing <1195223570@qq.com>
2019-09-28 15:29:15 +0800

    tomcat中Catalina加载server的方法

--- src/main/java/com/virgilin/digester/definition/DigesterRule.java
+++ src/main/java/com/virgilin/digester/definition/DigesterRule.java
@@ -25,8 +25,34 @@ public class DigesterRule {
         return digester.parse(url);
     }
 
+    /**
+     * tomcat中Catalina初始化server的方法
+     * @param filePath
+     * @return
+     * @throws IOException
+     * @throws SAXException
+     */
+    public SchoolDefinition execute2(String filePath) throws IOException, SAXException {
+        Digester digester = new Digester();
+        digester.setValidating(false);
+        //digester.addObjectCreate("school/classes",SchoolDefinition.class);
+        //digester.addSetProperties("school/classes/class");
+        digester.addObjectCreate("school/classes/class",ClassDefinition.class);
+        digester.addObjectCreate("school/classes/class/student",StudentDefinition.class);
+        digester.addBeanPropertySetter("school/classes/class/student/no");
+        digester.addBeanPropertySetter("school/classes/class/student/name");
+        digester.addBeanPropertySetter("school/classes/class/student/age");
+        digester.addSetNext("school/classes/class/student","addStudent");
+        digester.addSetNext("school/classes/class","addClass");
+        URL url = this.getClass().getClassLoader().getResource(filePath);
+        System.out.println("url=" + url.toString());
+        SchoolDefinition schoolDefinition = new SchoolDefinition();
+        digester.push(schoolDefinition);
+        return digester.parse(url);
+    }
+
     public static void main(String[] args) throws IOException, SAXException {
-        SchoolDefinition schoolDefinition = new DigesterRule().execute("school.xml");
+        SchoolDefinition schoolDefinition = new DigesterRule().execute2("school.xml");
         System.out.println(schoolDefinition);
     }
 }
